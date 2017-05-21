640466c11d8872329a4007bf6189122ae4efdf8b
huynhducbk95 <huynhducpro95@gmail.com>
2017-05-22 02:04:15 +0700

    xoa lung tung

--- Project3/elearning_system/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ElearningSystemConfig(AppConfig):
-    name = 'elearning_system'
--- Project3/elearning_system/user/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    print('x')
-    return render(request, 'elearning_system/user/index.html', {'title': 'registry'})
