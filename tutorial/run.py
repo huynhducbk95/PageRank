3b194e31a876ab0e6ce5a4b246f5df6f2d43e4f9
huynhducbk95 <huynhducpro95@gmail.com>
2017-09-18 00:27:03 +0700

    delete all

--- tutorial/run.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("scrapy crawl football -o football.json")
-os.system("scrapy crawl football -o football.json")
--- tutorial/tutorial/spiders/test.py
+++
@@ -1,7 +0,0 @@
-from scrapy import Selector
-from scrapy.http import HtmlResponse
-import scrapy
-
-
-class Test(scrapy.Item):
-    name = scrapy.Field(default='test', type=str, required=True)
