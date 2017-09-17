9f4fb4aecc0780b4b8d29a2af49c2331304507a5
huynhducbk95 <huynhducpro95@gmail.com>
2017-09-18 02:03:56 +0700

    change read file

--- utils.py
+++ utils.py
@@ -8,38 +8,20 @@ page_path = DIR_PATH + '/data/viwiki-20170901-page.sql'
 pagelink_path = DIR_PATH + '/data/viwiki-20170901-pagelinks.sql'
 
 
-def parse(page_path, pagelink_path):
-    page_file = open(page_path, "r")
-    pagelink_file = open(pagelink_path, 'r')
+def parse(pagelink_path):
+    file = open("pages.txt", "w")
 
-    ## use readlines to read all lines in the file
-    ## The variable "lines" is a list containing all lines
-    page_lines = page_file.readlines()
-    pagelink_lines = pagelink_file.readlines()
+    with open(pagelink_path) as infile:
+        for line in infile:
+            if line.split(' ')[0] == 'INSERT':
+                list_record = line.split('(')
+                # for record in list_record:
+                #     id = record.split(',')[0]
+                #
+                # file.write('id: {} - title: {} \n'.format(id, title))
+                for record in list_record:
+                    file.write('record: {} \n'.format(record))
 
-    page_count = 0
-    for line in page_lines:
-        if line.startswith('INSERT INTO'):
-            page_count = page_count + 1
+    file.close()
 
-    pagelink_count = 0
-    for line in pagelink_lines:
-        if line.startswith('INSERT INTO'):
-            pagelink_count = pagelink_count + 1
-
-
-    ## close the file after reading the lines.
-    page_file.close()
-    pagelink_file.close()
-
-
-parse(page_path, pagelink_path)
-
-
-def parse_page_line(line_sting):
-    list_record = line_sting.split('(')
-    
-    pass
-
-def parse_page_line(line_sting):
-    pass
+parse(pagelink_path)
