779086727abfa4ea7b292681b011732811473e91
huynhducbk95 <huynhducpro95@gmail.com>
2017-09-18 02:37:57 +0700

    change ad

--- utils.py
+++ utils.py
@@ -9,19 +9,34 @@ pagelink_path = DIR_PATH + '/data/viwiki-20170901-pagelinks.sql'
 
 
 def parse(pagelink_path):
-    file = open("pages.txt", "w")
+    file_page = open("pages.txt", "w")
 
     with open(pagelink_path) as infile:
         for line in infile:
             if line.split(' ')[0] == 'INSERT':
                 list_record = line.split('(')
-                # for record in list_record:
-                #     id = record.split(',')[0]
-                #
-                # file.write('id: {} - title: {} \n'.format(id, title))
                 for record in list_record:
-                    file.write('record: {} \n'.format(record))
+                    page_id = record.split(',')[0]
+                    page_namespace = record.split(',')[1]
+                    page_title = record.split(',')[2]
+                    file_page.write('id: {} -- nspace: {} -- title: {}\n'.format(page_id, page_namespace, page_title))
 
-    file.close()
+    file_page.close()
+
+    file_pagelink = open("pagelinks.txt", "w")
+
+    with open(pagelink_path) as infile:
+        for line in infile:
+            if line.split(' ')[0] == 'INSERT':
+                list_record = line.split('(')
+                for record in list_record:
+                    id_from = record.split(',')[0]
+                    namespace_from = record.split(',')[1]
+                    title_to = record.split(',')[2]
+                    namespace_to = record.split(',')[3]
+                    file_pagelink.write('{} \n'.format(record))
+
+    file_pagelink.close()
+
 
 parse(pagelink_path)
